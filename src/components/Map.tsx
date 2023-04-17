0bbf939063fddcf1dc9c7e48d72b9c66911789b7
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-04-17 08:27:01 -0400

    show location user has added

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,33 +1,20 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
-import {MarkerProps} from '../interfaces/MarkerInterface';
+import MapView from 'react-native-maps';
 
-interface Props {
-  markers?: MarkerProps[];
-}
-
-export const Map = ({markers}: Props) => {
+export const Map = () => {
   return (
     <View style={styles.container}>
       <MapView
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 8.618,
+          longitude: -71.143,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-        style={styles.container}>
-        {markers?.map(marker => (
-          <Marker
-            key={marker.title}
-            coordinate={marker.coordinate}
-            title={marker.title}
-            description={marker.description}
-            image={require('../assets/custom-marker.png')}
-          />
-        ))}
-      </MapView>
+        showsUserLocation
+        style={styles.container}
+      />
     </View>
   );
 };
--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,41 +1,13 @@
 import React from 'react';
 import {StyleSheet, SafeAreaView} from 'react-native';
 import {Map} from '../components/Map';
-import {MarkerProps} from '../interfaces/MarkerInterface';
-
-const maps: MarkerProps[] = [
-  {
-    coordinate: {
-      latitude: 37.8,
-      longitude: -122.4324,
-    },
-    title: 'Map1',
-    description: 'Description de Map 1',
-  },
-  {
-    coordinate: {
-      latitude: 37.78825,
-      longitude: -122.4354,
-    },
-    title: 'Map2',
-    description: 'Description de Map 2',
-  },
-  {
-    coordinate: {
-      latitude: 37.76,
-      longitude: -122.4365,
-    },
-    title: 'Map3',
-    description: 'Description de Map 2',
-  },
-];
 
 export const MapScreen = () => {
   const styles = stylesFunction();
 
   return (
     <SafeAreaView style={styles.container}>
-      <Map markers={maps} />
+      <Map />
     </SafeAreaView>
   );
 };
